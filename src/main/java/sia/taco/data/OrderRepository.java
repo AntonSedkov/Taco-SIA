9bdc236d81e4b9e9fa46bcab76bde7a4af35bac2
User from script <S3dk0vAG@gmail.com>
2021-10-24 12:14:33 +0300

    change to Spring Data Mongo DB + MongoDB in docker and for test + model markup with MongoDB annotations

--- src/main/java/sia/taco/data/OrderRepository.java
+++ src/main/java/sia/taco/data/OrderRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import sia.taco.model.TacoOrder;
 
-import java.util.UUID;
-
 @Repository
-public interface OrderRepository extends CrudRepository<TacoOrder, UUID> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 
 }
